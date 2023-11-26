7c23bad2eb45d5a836e5a954e9b7f41b37ae9a34
Byron Wall <byron@byroni.us>
2023-11-26 09:20:43 -0500

    Update styles for drag

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,17 +12,17 @@ const App = () => {
 
   return (
     <div className="relative" {...getRootProps()}>
-      <div
-        className={cn(
-          "absolute top-0 left-0 h-screen w-screen p-4 transition-all duration-300 ease-in-out -z-10",
-          {
-            "bg-gray-300 border-4 border-gray-500": isDragActive,
-          }
-        )}
-      ></div>
-      <div
-        className={cn("flex flex-col items-center justify-center min-h-screen")}
-      >
+      <div className="absolute top-0 left-0 h-screen w-screen flex items-stretch ">
+        <div
+          className={cn(
+            "m-1 p-4 transition-all duration-300 ease-in-out -z-10 flex-1 rounded-md",
+            {
+              "bg-gray-300 border-4 border-gray-500": isDragActive,
+            }
+          )}
+        ></div>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold">Image Converter</h1>
         <p className="text-xl mt-4">
           To make use of this tool, paste or drop something containing an image
