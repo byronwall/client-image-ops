bb5f2ba1694333c0ff7f532cd8bb6ee9885d00a4
Byron Wall <byron@byroni.us>
2023-11-30 22:35:34 -0500

    Remove quality

--- src/utils/heic.ts
+++ src/utils/heic.ts
@@ -5,16 +5,14 @@ import { extractBase64FromBlob, convertBase64ToUInt8Array } from "./buffers";
 export async function convertHEICToPNG(base64Data: string) {
   const uint8Array = convertBase64ToUInt8Array(base64Data);
 
-  // from buffer
   const blob = new Blob([uint8Array], { type: "image/heic" });
 
   let pngBlob = await heic2any({
     blob: blob,
     toType: "image/png",
-    quality: 0.8, // Optional: Adjust the quality
   });
 
-  // get single png blob
+  // get single png blob if heic2any returns an array
   if (Array.isArray(pngBlob)) {
     pngBlob = pngBlob[0];
   }
